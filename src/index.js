54db457db75c215b71bc0d9d76669ec7529dc882
Efraín González Castillo <efgonzalezca@gmail.com>
2022-08-06 18:15:00 -0500

    add events on, once and off

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const app = express();
 const httpServer = new HTTPServer(app);
 const io = new IOServer(httpServer);
 
-console.log(__dirname)
+const socketsOnline = [];
+
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
@@ -20,6 +21,8 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
 
+  socketsOnline.push(socket.id);
+
   socket.emit('welcome', 'Connection established');
 
   socket.on('server', (data) => {
@@ -27,6 +30,23 @@ io.on('connection', (socket) => {
   })
 
   io.emit('everyone', `${socket.id} has connected`);
+
+  socket.on('last', (data) => {
+    const lastSocket = socketsOnline[socketsOnline.length - 1];
+    io.to(lastSocket).emit('greeting', data);
+  })
+
+  socket.emit('on', 'Test on');
+  socket.emit('on', 'Test on');
+
+  socket.emit('once', 'Test once');
+  socket.emit('once', 'Test once');
+
+  socket.emit('off', 'Test');
+
+  setTimeout(() => {
+    socket.emit('off', 'Test');
+  }, 3000)
 })
 
 httpServer.listen(4500);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -14,3 +14,29 @@ emitToServer.addEventListener('click', () => {
 socket.on('everyone', (data) => {
   console.log(data);
 })
+const emitToLast = document.querySelector('#emit-to-last');
+emitToLast.addEventListener('click', () => {
+  socket.emit('last', 'The last one');
+})
+
+socket.on('greeting', (data) => {
+  console.log(data);
+})
+
+socket.on('on', (data) => {
+  console.log(data);
+})
+
+socket.once('once', (data) => {
+  console.log(data);
+})
+
+const listener = () => {
+  console.log('Turn off event');
+}
+
+socket.on('off', listener);
+
+setTimeout(() => {
+  socket.off('off', listener);
+}, 2000)
