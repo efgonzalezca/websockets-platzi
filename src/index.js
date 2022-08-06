a67262752ae5e5aabcf9f60c1b3447c616d7bb92
Efraín González Castillo <efgonzalezca@gmail.com>
2022-08-06 14:51:51 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import { Server as HTTPServer } from 'http';
+import { Server as IOServer } from 'socket.io';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const httpServer = new HTTPServer(app);
+const io = new IOServer(httpServer);
+
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+io.on('connection', (socket) => {
+  console.log(socket.id);
+})
+
+httpServer.listen(4500);
