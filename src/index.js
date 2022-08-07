f33c0ad233ffa32d96d8758eb3b1da7e0294244c
Efraín González Castillo <efgonzalezca@gmail.com>
2022-08-06 19:56:17 -0500

    add broadcast event

--- src/index.js
+++ src/index.js
@@ -11,8 +11,6 @@ const app = express();
 const httpServer = new HTTPServer(app);
 const io = new IOServer(httpServer);
 
-const socketsOnline = [];
-
 app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
@@ -20,33 +18,9 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-
-  socketsOnline.push(socket.id);
-
-  socket.emit('welcome', 'Connection established');
-
-  socket.on('server', (data) => {
-    console.log(data);
+  socket.on('circle-position', (data) => {
+    socket.broadcast.emit('move-circle', data);
   })
-
-  io.emit('everyone', `${socket.id} has connected`);
-
-  socket.on('last', (data) => {
-    const lastSocket = socketsOnline[socketsOnline.length - 1];
-    io.to(lastSocket).emit('greeting', data);
-  })
-
-  socket.emit('on', 'Test on');
-  socket.emit('on', 'Test on');
-
-  socket.emit('once', 'Test once');
-  socket.emit('once', 'Test once');
-
-  socket.emit('off', 'Test');
-
-  setTimeout(() => {
-    socket.emit('off', 'Test');
-  }, 3000)
 })
 
 httpServer.listen(4500);
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,42 +1,29 @@
 const socket = io();
 
-const text = document.querySelector('#text')
+const circle = document.querySelector('#circle');
 
-socket.on('welcome', (data) => {
-  text.textContent = data;
-})
-
-const emitToServer = document.querySelector('#emit-to-server');
-emitToServer.addEventListener('click', () => {
-  socket.emit('server', 'msg to server')
-})
-
-socket.on('everyone', (data) => {
-  console.log(data);
-})
-const emitToLast = document.querySelector('#emit-to-last');
-emitToLast.addEventListener('click', () => {
-  socket.emit('last', 'The last one');
-})
-
-socket.on('greeting', (data) => {
-  console.log(data);
-})
-
-socket.on('on', (data) => {
-  console.log(data);
-})
-
-socket.once('once', (data) => {
-  console.log(data);
-})
-
-const listener = () => {
-  console.log('Turn off event');
+const drawCircle = ({ top, left }) => {
+  circle.style.top = top;
+  circle.style.left = left;
 }
 
-socket.on('off', listener);
+const drag = (e) => {
+  const position = {
+    top: e.clientY + 'px',
+    left: e.clientX + 'px'
+  };
+  drawCircle(position);
+  socket.emit('circle-position', position);
+}
 
-setTimeout(() => {
-  socket.off('off', listener);
-}, 2000)
+document.addEventListener('mousedown', (e) => {
+  document.addEventListener('mousemove', drag)
+})
+
+document.addEventListener('mouseup', (e) => {
+  document.removeEventListener('mousemove', drag)
+})
+
+socket.on('move-circle', ({ top, left }) => {
+  drawCircle({ top, left });
+})
