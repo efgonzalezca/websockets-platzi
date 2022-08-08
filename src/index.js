31c646755509212a1eb3ee1d05f900da788b38a3
Efraín González Castillo <efgonzalezca@gmail.com>
2022-08-07 20:25:56 -0500

    add socket admin-ui

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import { fileURLToPath } from 'url';
 import express from 'express';
 import { Server as HTTPServer } from 'http';
 import { Server as IOServer } from 'socket.io';
+import { instrument } from '@socket.io/admin-ui';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const httpServer = new HTTPServer(app);
-const io = new IOServer(httpServer);
+const io = new IOServer(httpServer, {
+  cors: {
+    origin: ['https:admin.socket.io'],
+    credentials: true
+  }
+});
+
+instrument(io, {
+  auth: {
+    type: 'basic',
+    username: 'admin',
+    password: '$2b$10$rIUoP06.v80Ob9Q58Pkcp.73QBRlTzBUj0WUiRHe/E2MoNPSvVN7S'
+  }
+})
 
 app.use(express.static(path.join(__dirname, 'views')));
 
@@ -17,21 +31,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
-io.use((socket, next) => {
-  const token = socket.handshake.auth.token;
-  if(token === 'SECRET_KEY') {
-    next();
-  } else {
-    const err = new Error('Unauthorized');
-    err.data = {
-      details: 'Could not be authenticated'
-    }
-    next(err);
-  }
-});
-
 io.on('connection', (socket) => {
-  console.log(socket.id);
+  socket.on('circle-position', (data) => {
+    socket.broadcast.emit('move-circle', data);
+  });
 });
 
 
--- src/views/js/index.js
+++ src/views/js/index.js
@@ -1,11 +1,30 @@
-const socket = io({
-  auth: {
-    token: 'SECRET_KEY'
-  }
-});
+const socket = io();
 
-socket.on('connect_error', (err) => {
-  console.log('Connection failed');
-  console.log(err.message);
-  console.log(err.data.details);
+const circle = document.querySelector('#circle');
+
+const drawCircle = ({ top, left }) => {
+  circle.style.top = top;
+  circle.style.left = left;
+}
+
+const drag = (e) => {
+  const position = {
+    top: e.clientY + 'px',
+    left: e.clientX + 'px'
+  };
+  drawCircle(position);
+  console.log('Event sends to server');
+  socket.volatile.emit('circle-position', position);
+}
+
+document.addEventListener('mousedown', (e) => {
+  document.addEventListener('mousemove', drag)
 })
+
+document.addEventListener('mouseup', (e) => {
+  document.removeEventListener('mousemove', drag)
+})
+
+socket.on('move-circle', ({ top, left }) => {
+  drawCircle({ top, left });
+})  
